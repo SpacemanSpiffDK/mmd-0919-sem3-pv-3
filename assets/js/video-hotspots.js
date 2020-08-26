e7c8b4df8db410949517301b2263e2e93ffb5785
Dan Høegh <dan.hoegh@gmail.com>
2020-08-26 11:04:23 +0200

    added clean examples on video1

--- assets/js/video-hotspots.js
+++ assets/js/video-hotspots.js
@@ -1,8 +1,8 @@
 // this script assumes video.js for finding HTML DOM elements (parent DIVs to VIDEO tags)
 
 // ## SETTINGS START
-const debug = false; // ## set to true to get console.log output, use   video.log('text')
 const fps = 30;     // ## adjust this to set the frames per second precision on the hotspot appearance (lower = less cpu used)
+const debug = false; // ## set to true to get console.log output, use   video.log('text')
 // ## SETTINGS END
 
 const msInterval = Math.floor(1000/fps); // calculate how many ms per loop to match desired FPS. Rounded down
@@ -85,6 +85,9 @@ let video = {
                                 let elmHotspot = document.createElement('a');
                                 elmHotspot.id = `hotspotId${index}`;
                                 elmHotspot.className = 'hotspot';
+                                if (hotspot.ui.title){
+                                    elmHotspot.title = hotspot.ui.title;
+                                }
                                 let css = "";
                                 css += `width: ${hotspot.sizeX}%;`;
                                 css += `height: ${hotspot.sizeY}%;`;
@@ -131,14 +134,15 @@ let video = {
 
 const hotspots = [
     {
+        // bird strike scream
         active: true,
         videoId: "video1",
         markIn: 20.4,
         markOut: 21,
-        posX: 52,
-        posY: 6,
         sizeX: 30,
         sizeY: 32,
+        posX: 52,
+        posY: 6,
         ui: {
             type: "image",
             image: "assets/images/speech-scream.png",
@@ -146,10 +150,76 @@ const hotspots = [
         },
         hotspot: {
             type: "function",
+            onHover: true,
             func: function () {
-                // run any javascript you want done when clicking on the hotspot
-                // Leave empty if you want nothing to happen
+                alert('Head trauma detected, administering morphine');
             }
+        }
+    },
+    {
+        // title text
+        active: true,
+        videoId: "video1",
+        markIn: 24.5,
+        markOut: 29.5,
+        sizeX: 49,
+        sizeY: 39,
+        posX: 27,
+        posY: 19,
+        ui: {
+            type: "box",
+            title: "Visit the blender website",
+            boxBorder: "none",
+            boxBackgroundColor: "rgba(0,0,0,.1)"
+        },
+        hotspot: {
+            type: "link",
+            url: "https://peach.blender.org/",
+            target: "_blank"
+        }
+    },
+    {
+        // text right after the bird strike
+        active: true,
+        videoId: "video1",
+        markIn: 20.3,
+        markOut: 23.0,
+        sizeX: 55,
+        sizeY: 19,
+        posX: 22,
+        posY: 40,
+        ui: {
+            type: "box",
+            title: "Visit the blender website",
+            boxBorder: "none",
+            boxBackgroundColor: "rgba(0,0,0,.1)"
+        },
+        hotspot: {
+            type: "link",
+            url: "https://peach.blender.org/",
+            target: "_blank"
+        }
+    },
+    {
+        // Coffee advert
+        active: true,
+        videoId: "video1",
+        markIn: 36.0,
+        markOut: 47.0,
+        sizeX: 20,
+        sizeY: 42,
+        posX: 65,
+        posY: 24,
+        ui: {
+            type: "image",
+            title: "Get coffee!!!",
+            image: "assets/images/get-coffee.png",
+            boxBorder: "none"
+        },
+        hotspot: {
+            type: "link",
+            url: "https://coffeecollective.dk/da/shop/",
+            target: "_blank"
         }
     },
     {
@@ -157,10 +227,10 @@ const hotspots = [
         videoId: "video2",
         markIn: 4,
         markOut: 10,
-        posX: 50,
-        posY: 25,
         sizeX: 20,
         sizeY: 20,
+        posX: 50,
+        posY: 25,
         ui: {
             type: "box",
             image: "",
@@ -178,10 +248,10 @@ const hotspots = [
         videoId: "video2",
         markIn: 5,
         markOut: 11,
-        posX: 60,
-        posY: 30,
         sizeX: 20,
         sizeY: 20,
+        posX: 60,
+        posY: 30,
         ui: {
             type: "box",
             image: "",
@@ -199,10 +269,10 @@ const hotspots = [
         videoId: "video3",
         markIn: 3,
         markOut: 5,
-        posX: 50,
-        posY: 25,
         sizeX: 10,
         sizeY: 10,
+        posX: 50,
+        posY: 25,
         ui: {
             type: "box",
             image: "",
@@ -220,10 +290,10 @@ const hotspots = [
         videoId: "video3",
         markIn: 4,
         markOut: 6,
-        posX: 70,
-        posY: 50,
         sizeX: 20,
         sizeY: 20,
+        posX: 70,
+        posY: 50,
         ui: {
             type: "box",
             image: "",
@@ -241,10 +311,10 @@ const hotspots = [
         videoId: "video4",
         markIn: 2,
         markOut: 3,
-        posX: 0,
-        posY: 0,
         sizeX: 50,
         sizeY: 50,
+        posX: 0,
+        posY: 0,
         ui: {
             type: "box",
             image: "",
@@ -262,10 +332,10 @@ const hotspots = [
         videoId: "video4",
         markIn: 3,
         markOut: 4,
-        posX: 50,
-        posY: 0,
         sizeX: 50,
         sizeY: 50,
+        posX: 50,
+        posY: 0,
         ui: {
             type: "box",
             image: "",
@@ -283,10 +353,10 @@ const hotspots = [
         videoId: "video4",
         markIn: 4,
         markOut: 5,
-        posX: 50,
-        posY: 50,
         sizeX: 50,
         sizeY: 50,
+        posX: 50,
+        posY: 50,
         ui: {
             type: "box",
             image: "",
@@ -304,10 +374,10 @@ const hotspots = [
         videoId: "video4",
         markIn: 5,
         markOut: 6,
-        posX: 0,
-        posY: 50,
         sizeX: 50,
         sizeY: 50,
+        posX: 0,
+        posY: 50,
         ui: {
             type: "box",
             image: "",
@@ -322,3 +392,91 @@ const hotspots = [
     }
 ];
 
+/*
+
+// MIXED EXAMPLE WITH MORE KEY/VALUE PAIRS THAN YOU REALLY NEED
+
+{
+    active: true,           // you can set this to false if you don't want this hotspot to appear
+    videoId: "video4",      // the id of the video tag you want to add a hotspot to
+    markIn: 5,              // when to start (seconds)
+    markOut: 6,             // when to end (seconds)
+    sizeX: 50,              // width (in %, but don't write %)
+    sizeY: 50,              // height (in %, but don't write %)
+    posX: 0,                // left position (in %, but don't write %)
+    posY: 50,               // top position (in %, but don't write %)
+    ui: {                               // how should the hotspot look?
+        type: "box",                                // type: "box" or "image"
+        title: "Text when hovering the hotspot",    // optional: add a title attribute with the text 
+        image: "",                                  // add url for image (if type=image)
+        boxBorder: "none",                          // add styles for border, can be used for both image and box
+        boxBackgroundColor: "rgba(0,255,0,.5)"      // add style for background color (if type=box)
+    },
+    hotspot: {                          // what should the hotspot do when clicked?
+        type: "link",                               // type: "link" or "function"
+        onHover: true,                              // trigger on hover (if type=function)
+        url: "http://tv2.dk",                       // url (if type=link)
+        target: "_blank",                           // target (if type=link)
+        func: function () {             // (if type=function)
+            // run any javascript you want done when clicking on the hotspot
+            // Leave empty if you want nothing to happen
+        }
+    }
+}
+
+// BOX/LINK ONLY EXAMPLE ----------------------------------------------------
+// All these keys are required for boxes/links
+
+{
+    active: true,
+    videoId: "video4",
+    markIn: 5,
+    markOut: 6,
+    sizeX: 50,
+    sizeY: 50,
+    posX: 0,
+    posY: 50,
+    ui: {
+        type: "box",
+        title: "Text when hovering the hotspot",    // optional: add a title attribute with the text
+        boxBorder: "2px solid green",
+        boxBackgroundColor: "rgba(0,255,0,.5)"
+    },
+    hotspot: {
+        type: "link",
+        url: "http://tv2.dk",
+        target: "_blank"
+    }
+}
+
+
+// IMAGE/FUNCTION ONLY EXAMPLE ----------------------------------------------------
+// All these keys are required for images/functions
+
+const hotspots = [
+    {
+        active: true,
+        videoId: "video1",
+        markIn: 20.4,
+        markOut: 21,
+        sizeX: 30,
+        sizeY: 32,
+        posX: 52,
+        posY: 6,
+        ui: {
+            type: "image",
+            title: "Text when hovering the hotspot",    // optional: add a title attribute with the text
+            image: "assets/images/speech-scream.png",
+            boxBorder: "none"
+        },
+        hotspot: {
+            type: "function",
+            onHover: true,      // optional, will default to false (trigger function on click)
+            func: function () {
+                console.log("Internal screaming!");
+            }
+        }
+    },
+
+
+*/
