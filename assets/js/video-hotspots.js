98bd38dcf1d91336da859c0ddcf430da4e01900c
Dan Høegh <dan.hoegh@gmail.com>
2020-08-28 11:55:14 +0200

    Update video-hotspots.js

--- assets/js/video-hotspots.js
+++ assets/js/video-hotspots.js
@@ -1,3 +1,6 @@
+// JS by Dan Høegh
+// UCN MMD 2020
+
 // A framework for showing time-encoded hotspots on multiple videos.
 // Assumes either video.js or a parent <div> for the video that has the same dimensions as the video.
 
